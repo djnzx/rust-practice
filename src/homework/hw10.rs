b4ae161a3fe79f00415f0c258a1fb818d570f524
M8L8X <Artem.Kovtun@e-u.edu.ua>
2025-04-30 15:30:14 +0300

    Практична 10

---
+++ src/homework/hw10.rs
@@ -0,0 +1,29 @@
+fn is_palindrome(x: u32) -> bool {
+    let s = x.to_string();
+    s.chars().eq(s.chars().rev())
+}
+
+fn main() {
+    let nums = [123, 121, 1221, 101, 10];
+    for n in nums {
+        println!("{} -> {}", n, is_palindrome(n));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let data = [
+            (123, false),
+            (121, true),
+            (1221, true),
+        ];
+
+        data.iter().for_each(|(n, exp)| {
+            assert_eq!(is_palindrome(*n), *exp);
+        });
+    }
+}
