aeee1b48b0c8c7e517abd573a29c0a2ec981091f
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2025-05-08 15:23:38 +0300

    envelope

--- src/homework/hw01.rs
+++ src/homework/hw01.rs
@@ -1,4 +1,18 @@
 #[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+fn test_draw_envelope() {
+    draw_envelope(30, 15);
+}
+
+fn draw_envelope(width: u8, height: u8) {
+    for y in 0..height {
+        for x in 0..width {
+            let hor = y == 0 || y == height - 1;
+            let ver = x == 0 || x == width - 1;
+            let dia = x == y;
+            
+            let sym = if hor || ver || dia { '*' } else { ' ' };
+            print!("{sym}");
+        }
+        println!();
+    }
 }
